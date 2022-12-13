ed89bd88cec90e58fc7ab9913369fa8801ec0f2c
Shendy Krisyohanda <krisyohanda@gmail.com>
2022-12-13 10:50:17 +0800

    Add Final Project

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+import numpy as np
+import pickle 
+
+app = flask.Flask(__name__, template_folder="templates")
+
+model = pickle.load(open('model/model_classifier.pkl','rb'))
+
+@app.route('/')
+def index():
+    return(flask.render_template('main.html'))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_feature = [int(val)for val in flask.request.form.values()]
+    final_feature = [np.array(int_feature)]
+    prediction = model.predict(final_feature)
+    output = {0: 'Tidak Hujan', 1: 'Hari Ini Hujan'}
+
+    return flask.render_template('main.html', prediction_text = '{}'.format(output[prediction[0]]))
+
+if __name__ == '__main__':
+    app.run(debug=True)
