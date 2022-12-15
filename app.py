6bba365a5dcc62b87530e4ff5d3fa8490e8bee7a
Shendy Krisyohanda <krisyohanda@gmail.com>
2022-12-15 14:50:23 +0800

    Revisi UI

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
 import flask
 import numpy as np
 import pickle 
+from sklearn.preprocessing import StandardScaler
 
 app = flask.Flask(__name__, template_folder="templates")
 
 model = pickle.load(open('model/model_classifier.pkl','rb'))
 
+scaler = StandardScaler()
+
 @app.route('/')
 def index():
-    return(flask.render_template('main.html'))
+    return(flask.render_template('index.html'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_feature = [int(val)for val in flask.request.form.values()]
-    final_feature = [np.array(int_feature)]
-    prediction = model.predict(final_feature)
-    output = {0: 'Tidak Hujan', 1: 'Hari Ini Hujan'}
+    #  <!-- ['WindGustSpeed', 'Humidity9am', 'Humidity3pm', 'Cloud9am', 'Cloud3pm','RainToday']  --> 
+    
+    WindGustSpeed = float(flask.request.form["WindGustSpeed"])
+    Humidity9am = float(flask.request.form["Humidity9am"])
+    Humidity3pm = float(flask.request.form["Humidity3pm"])
+    Cloud9am = float(flask.request.form["Cloud9am"])
+    Cloud3pm = float(flask.request.form["Cloud3pm"])
+    RainToday = int(flask.request.form["RainToday"])
 
-    return flask.render_template('main.html', prediction_text = '{}'.format(output[prediction[0]]))
+    data_list = [[
+        WindGustSpeed,
+        Humidity9am,
+        Humidity3pm,
+        Cloud9am,
+        Cloud3pm,
+        RainToday
+    ]]
+
+    final_feature = [np.array(data_list)]
+    pred_scaller = scaler.fit_transform(final_feature)
+    prediction = model.predict(pred_scaller)
+    output = {0: 'Tidak Hujan', 1: 'Hujan'}
+
+    return flask.render_template('index.html', prediction_text = 'Prediksi cuaca besok di Australia adalah {}'.format(output[prediction[0]]))
 
 if __name__ == '__main__':
     app.run(debug=True)
